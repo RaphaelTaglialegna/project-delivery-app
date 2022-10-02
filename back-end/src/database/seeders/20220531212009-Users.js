12ced4e1f304bcc5894c1266961acc57bfecc373
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-10-02 18:11:20 -0300

    Senhas exemplo e docker compose

--- back-end/src/database/seeders/20220531212009-Users.js
+++ back-end/src/database/seeders/20220531212009-Users.js
@@ -1,38 +1,42 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkInsert('users',
-      [{
-        id: 1,
-        name: 'Delivery App Admin',
-        email: 'adm@deliveryapp.com',
-        password: 'a4c86edecc5aee06eff8fdeda69e0d04',
-        role: 'administrator',
-      },
-      {
-        id: 2,
-        name: 'Fulana Pereira',
-        email: 'fulana@deliveryapp.com',
-        password: '3c28d2b0881bf46457a853e0b07531c6',
-        role: 'seller',
-      },
-      {
-        id: 3,
-        name: 'Cliente Zé Birita',
-        email: 'zebirita@email.com',
-        password: '1c37466c159755ce1fa181bd247cb925',
-        role: 'customer',
-      },
-      {
-        id: 4,
-        name: 'Teste Teste Teste',
-        email: 'teste@teste.com',
-        password: 'e10adc3949ba59abbe56e057f20f883e',
-        role: 'customer',
-      },
-      ], { timestamps: false });
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          id: 1,
+          name: "Delivery App Admin",
+          email: "adm@deliveryapp.com",
+          password: "a4c86edecc5aee06eff8fdeda69e0d04", //--adm2@21!!--
+          role: "administrator",
+        },
+        {
+          id: 2,
+          name: "Fulana Pereira",
+          email: "fulana@deliveryapp.com",
+          password: "3c28d2b0881bf46457a853e0b07531c6", //fulana@123
+          role: "seller",
+        },
+        {
+          id: 3,
+          name: "Cliente Zé Birita",
+          email: "zebirita@email.com",
+          password: "1c37466c159755ce1fa181bd247cb925", //$#zebirita#$
+          role: "customer",
+        },
+        {
+          id: 4,
+          name: "Teste Teste Teste",
+          email: "teste@teste.com",
+          password: "e10adc3949ba59abbe56e057f20f883e",
+          role: "customer",
+        },
+      ],
+      { timestamps: false }
+    );
   },
-  
+
   down: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete("users", null, {});
   },
 };
